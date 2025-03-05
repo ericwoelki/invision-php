416b62a21c17f31ea7bf38f2b868642d0596401e
Eric Woelki <ericwoelki@gmail.com>
2025-03-05 04:47:49 -0500

    style: missing function return type

--- tests/Feature/Requests/Forums/GetForumRequestTest.php
+++ tests/Feature/Requests/Forums/GetForumRequestTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use EricWoelki\Invision\Requests\Forums\GetForumRequest;
 
-it('resolves the endpoint', function () {
+it('resolves the endpoint', function (): void {
     $request = new GetForumRequest(1);
 
     expect($request->resolveEndpoint())->toBe('forums/forums/1');

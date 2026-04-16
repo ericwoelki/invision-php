e94fc7bb830a9fc9cd6fed8dfc8b4b841d76f885
Eric Woelki <ericwoelki@gmail.com>
2026-04-15 22:02:37 -0400

    fix(forums): forum permission parsing

--- src/Data/Permissions.php
+++ src/Data/Permissions.php
@@ -8,38 +8,43 @@ namespace EricWoelki\Invision\Data;
 final readonly class Permissions
 {
     /**
-     * @param  list<int>  $perm_view
-     * @param  list<int>  $perm_2
-     * @param  list<int>  $perm_3
-     * @param  list<int>  $perm_4
-     * @param  list<int>  $perm_5
-     * @param  list<int>  $perm_6
-     * @param  list<int>  $perm_7
+     * @param  list<int>|string  $perm_view
+     * @param  list<int>|string  $perm_2
+     * @param  list<int>|string  $perm_3
+     * @param  list<int>|string  $perm_4
+     * @param  list<int>|string  $perm_5
+     * @param  list<int>|string  $perm_6
+     * @param  list<int>|string  $perm_7
      */
     public function __construct(
         public int $perm_id,
-        public array $perm_view,
-        public array $perm_2,
-        public array $perm_3,
-        public array $perm_4,
-        public array $perm_5,
-        public array $perm_6,
-        public array $perm_7,
+        public array|string $perm_view,
+        public array|string $perm_2,
+        public array|string $perm_3,
+        public array|string $perm_4,
+        public array|string $perm_5,
+        public array|string $perm_6,
+        public array|string $perm_7,
     ) {}
 
     /** @param PermissionsData $data */
     public static function fromArray(array $data): Permissions
     {
-        $groupPermissionIds = fn (string $permissions): array => array_map(fn (string $id): int => (int) $id, explode(',', $permissions));
+        $evaluatePermissions = fn (?string $permissions): array|string => match ($permissions) {
+            null => [],
+            '*' => '*',
+            default => array_map(fn (string $id): int => (int) $id, explode(',', $permissions)),
+        };
 
-        return new self(...array_merge($data, [
-            'perm_view' => $groupPermissionIds($data['perm_view']),
-            'perm_2' => $data['perm_2'] ? $groupPermissionIds($data['perm_2']) : [],
-            'perm_3' => $data['perm_3'] ? $groupPermissionIds($data['perm_3']) : [],
-            'perm_4' => $data['perm_4'] ? $groupPermissionIds($data['perm_4']) : [],
-            'perm_5' => $data['perm_5'] ? $groupPermissionIds($data['perm_5']) : [],
-            'perm_6' => $data['perm_6'] ? $groupPermissionIds($data['perm_6']) : [],
-            'perm_7' => $data['perm_7'] ? $groupPermissionIds($data['perm_7']) : [],
-        ]));
+        return new self(
+            perm_id: $data['perm_id'],
+            perm_view: $evaluatePermissions($data['perm_view']),
+            perm_2: $evaluatePermissions($data['perm_2']),
+            perm_3: $evaluatePermissions($data['perm_3']),
+            perm_4: $evaluatePermissions($data['perm_4']),
+            perm_5: $evaluatePermissions($data['perm_5']),
+            perm_6: $evaluatePermissions($data['perm_6']),
+            perm_7: $evaluatePermissions($data['perm_7']),
+        );
     }
 }

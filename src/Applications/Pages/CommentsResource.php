479067797d367e0582b2287fad78c57977c70fbd
Eric Woelki <ericwoelki@gmail.com>
2026-04-17 23:50:26 -0400

    feat(pages): support `POST /cms/comments/{database}` endpoint

--- src/Applications/Pages/CommentsResource.php
+++ src/Applications/Pages/CommentsResource.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace EricWoelki\Invision\Applications\Pages;
 
+use EricWoelki\Invision\Applications\Pages\Payloads\CreateCommentPayload;
 use EricWoelki\Invision\Applications\Pages\Payloads\ListCommentsPayload;
+use EricWoelki\Invision\Applications\Pages\Requests\CreateCommentRequest;
 use EricWoelki\Invision\Applications\Pages\Requests\GetCommentRequest;
 use EricWoelki\Invision\Applications\Pages\Requests\ListCommentsRequest;
 use EricWoelki\Invision\Data\Comment;
@@ -23,6 +25,13 @@ final class CommentsResource extends BaseResource
     public function get(int $databaseId, int $commentId): Comment
     {
         $request = new GetCommentRequest($databaseId, $commentId);
+
+        return $request->createDtoFromResponse($this->connector->send($request));
+    }
+
+    public function create(CreateCommentPayload $payload): Comment
+    {
+        $request = new CreateCommentRequest($payload);
 
         return $request->createDtoFromResponse($this->connector->send($request));
     }
--- tests/Feature/Applications/Pages/CommentsResourceTest.php
+++ tests/Feature/Applications/Pages/CommentsResourceTest.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use EricWoelki\Invision\Applications\Pages\Payloads\CreateCommentPayload;
 use EricWoelki\Invision\Applications\Pages\Payloads\ListCommentsPayload;
+use EricWoelki\Invision\Applications\Pages\Requests\CreateCommentRequest;
 use EricWoelki\Invision\Applications\Pages\Requests\GetCommentRequest;
 use EricWoelki\Invision\Applications\Pages\Requests\ListCommentsRequest;
 use EricWoelki\Invision\Data\Comment;
@@ -35,4 +37,21 @@ it('gets a comment', function (): void {
     $comment = $this->invision->pages()->comments()->get(databaseId: 1, commentId: 1);
 
     expect($comment)->toBeInstanceOf(Comment::class);
+});
+
+it('creates a comment', function (): void {
+    MockClient::global([
+        CreateCommentRequest::class => new InvisionFixture('pages/comments/create'),
+    ]);
+
+    $comment = $this->invision->pages()->comments()->create(new CreateCommentPayload(
+        databaseId: 1,
+        recordId: 2,
+        authorId: 2,
+        content: '::content::',
+    ));
+
+    expect($comment)
+        ->toBeInstanceOf(Comment::class)
+        ->and($comment->content)->toBe('::content::');
 });

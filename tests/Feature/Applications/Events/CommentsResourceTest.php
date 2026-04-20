0ed1256b824f1dd5f8df811171b66d4146a07978
Eric Woelki <ericwoelki@gmail.com>
2026-04-20 02:35:32 -0400

    feat(events): support `POST /calendar/comments` endpoint

--- tests/Feature/Applications/Events/CommentsResourceTest.php
+++ tests/Feature/Applications/Events/CommentsResourceTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use EricWoelki\Invision\Applications\Events\Payloads\CreateCommentPayload;
+use EricWoelki\Invision\Applications\Events\Requests\CreateCommentRequest;
 use EricWoelki\Invision\Applications\Events\Requests\GetCommentRequest;
 use EricWoelki\Invision\Applications\Events\Requests\ListCommentsRequest;
 use EricWoelki\Invision\Data\Comment;
@@ -32,4 +34,20 @@ it('gets a comment', function (): void {
     $comment = $this->invision->events()->comments()->get(1);
 
     expect($comment)->toBeInstanceOf(Comment::class);
+});
+
+it('creates a comment', function (): void {
+    MockClient::global([
+        CreateCommentRequest::class => new InvisionFixture('events/comments/create'),
+    ]);
+
+    $comment = $this->invision->events()->comments()->create(new CreateCommentPayload(
+        eventId: 2,
+        authorId: 1,
+        content: '::content::',
+    ));
+
+    expect($comment)
+        ->toBeInstanceOf(Comment::class)
+        ->and($comment->content)->toBe('::content::');
 });

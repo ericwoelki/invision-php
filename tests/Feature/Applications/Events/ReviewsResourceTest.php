16f981b4cc655c27db7e8e966d24a811d34e0f16
Eric Woelki <ericwoelki@gmail.com>
2026-04-20 20:47:09 -0400

    feat(events): support `POST /calendar/reviews/{id}` endpoint

--- tests/Feature/Applications/Events/ReviewsResourceTest.php
+++ tests/Feature/Applications/Events/ReviewsResourceTest.php
@@ -3,9 +3,11 @@
 declare(strict_types=1);
 
 use EricWoelki\Invision\Applications\Events\Payloads\CreateReviewPayload;
+use EricWoelki\Invision\Applications\Events\Payloads\UpdateReviewPayload;
 use EricWoelki\Invision\Applications\Events\Requests\CreateReviewRequest;
 use EricWoelki\Invision\Applications\Events\Requests\GetReviewRequest;
 use EricWoelki\Invision\Applications\Events\Requests\ListReviewsRequest;
+use EricWoelki\Invision\Applications\Events\Requests\UpdateReviewRequest;
 use EricWoelki\Invision\Data\Review;
 use Saloon\Http\Faking\MockClient;
 use Tests\Fixtures\InvisionFixture;
@@ -51,4 +53,19 @@ it('creates a review', function (): void {
     expect($review)
         ->toBeInstanceOf(Review::class)
         ->and($review->content)->toBe('::content::');
+});
+
+it('updates a review', function (): void {
+    MockClient::global([
+        UpdateReviewRequest::class => new InvisionFixture('events/reviews/update'),
+    ]);
+
+    $review = $this->invision->events()->reviews()->update(new UpdateReviewPayload(
+        reviewId: 2,
+        content: '::edited::',
+    ));
+
+    expect($review)
+        ->toBeInstanceOf(Review::class)
+        ->and($review->content)->toBe('::edited::');
 });
